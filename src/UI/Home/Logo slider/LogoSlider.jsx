fda6e43df136ba04255ec06160a1cc8940282da6
shaikot07 <saifulislamshaikat007@gmail.com>
2024-06-06 23:24:57 +0600

    added slider images

--- src/UI/Home/Logo slider/LogoSlider.jsx
+++ src/UI/Home/Logo slider/LogoSlider.jsx
@@ -2,6 +2,18 @@
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 // import "./styles.css"
+// import images 
+import sl1 from '../../../assets/sl1.svg'
+import sl2 from '../../../assets/sl2.svg'
+import sl3 from '../../../assets/sl3.svg'
+import sl4 from '../../../assets/sl4.svg'
+import sl5 from '../../../assets/sl5.svg'
+import sl6 from '../../../assets/sl6.svg'
+import sl7 from '../../../assets/sl7.svg'
+import sl8 from '../../../assets/sl8.svg'
+import sl9 from '../../../assets/sl9.svg'
+import sl10 from '../../../assets/sl10.svg'
+import sl11 from '../../../assets/sl10.svg'
 
 const animation = { duration: 5000, easing: (t) => t }
 
@@ -23,12 +35,44 @@ const LogoSlider = () => {
     return (
         <div>
             <div ref={sliderRef} className="keen-slider">
-                <div className="keen-slider__slide number-slide1">1</div>
-                <div className="keen-slider__slide number-slide2">2</div>
-                <div className="keen-slider__slide number-slide3">3</div>
-                <div className="keen-slider__slide number-slide4">4</div>
-                <div className="keen-slider__slide number-slide5">5</div>
-                <div className="keen-slider__slide number-slide6">6</div>
+                <div className="keen-slider__slide number-slide1">
+                    <img className="w-[250px]" src={ sl1} alt="" />
+                </div>
+                <div className="keen-slider__slide number-slide1">
+                    <img className="w-[250px]" src={ sl2} alt="" />
+                </div>
+                <div className="keen-slider__slide number-slide1">
+                    <img className="w-[250px]" src={ sl3} alt="" />
+                </div>
+                <div className="keen-slider__slide number-slide1">
+                    <img className="w-[250px]" src={ sl4} alt="" />
+                </div>
+                <div className="keen-slider__slide number-slide1">
+                    <img className="w-[250px]" src={ sl5} alt="" />
+                </div>
+                <div className="keen-slider__slide number-slide1">
+                    <img className="w-[250px]" src={ sl6} alt="" />
+                </div>
+                <div className="keen-slider__slide number-slide1">
+                    <img className="w-[250px]" src={ sl7} alt="" />
+                </div>
+                
+                <div className="keen-slider__slide number-slide1">
+                    <img className="w-[250px]" src={ sl8} alt="" />
+                </div>
+                
+                <div className="keen-slider__slide number-slide1">
+                    <img className="w-[250px]" src={ sl9} alt="" />
+                </div>
+                
+                <div className="keen-slider__slide number-slide1">
+                    <img className="w-[250px]" src={ sl10} alt="" />
+                </div>
+                
+                <div className="keen-slider__slide number-slide1">
+                    <img className="w-[250px]" src={ sl11} alt="" />
+                </div>
+                
             </div>
         </div>
     );
