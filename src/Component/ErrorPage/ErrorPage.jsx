aff76e4203d23c907288b9f14c303fc7d6093a6e
shaikot07 <saifulislamshaikat007@gmail.com>
2024-09-03 21:00:04 +0600

    updated

--- src/Component/ErrorPage/ErrorPage.jsx
+++ src/Component/ErrorPage/ErrorPage.jsx
@@ -4,6 +4,7 @@ const ErrorPage = () => {
     return (
         <div className='max-w-5xl mx-auto mt-4'>
            <h2 className='mt-12 text-red-600 text-2xl text-center font-semibold'>Ooooops...... Data Not Found ! </h2>
+           <h2 className='mt-12 text-red-600 text-2xl text-center font-semibold'>Back to Home </h2>
         </div>
     );
 };
